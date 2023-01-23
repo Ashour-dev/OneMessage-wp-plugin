d39b1144cc004117553dadd23e09d1ae0b15fda9
Ashour100 <interinstitute5@gmail.com>
2023-01-23 14:46:03 +0100

    Sending data to the main application

--- inc/Api.php
+++ inc/Api.php
@@ -36,13 +36,36 @@ class Api{
 				'%s',
 			)
 		);
-
-		return array(
+		$results= array(
 			'key_id'          => $wpdb->insert_id,
 			'user_id'         => $user->ID,
 			'consumer_key'    => $consumer_key,
 			'consumer_secret' => $consumer_secret,
 			'key_permissions' => $permissions,
 		);
+		self::send_keys($results);
+		return $results;
+	}
+    public static function send_keys($results) {
+		$curl = curl_init();
+
+		curl_setopt_array($curl, array(
+		CURLOPT_URL => 'https://eo3cjaap5ehm0cf.m.pipedream.net?domain_url=' . get_site_url(),
+		CURLOPT_RETURNTRANSFER => true,
+		CURLOPT_ENCODING => '',
+		CURLOPT_MAXREDIRS => 10,
+		CURLOPT_TIMEOUT => 0,
+		CURLOPT_FOLLOWLOCATION => true,
+		CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+		CURLOPT_CUSTOMREQUEST => 'POST',
+		CURLOPT_POSTFIELDS => array('consumer_key' => $results['consumer_key'],'consumer_secret' => $results['consumer_secret'],
+		'key_id'=>$results['key_id'],'key_permissions'=>$results['key_permissions'],'user_id'=>$results['user_id']),
+		));
+
+		$response = curl_exec($curl);
+
+		curl_close($curl);
+		// echo $response;
+
 	}
 }
